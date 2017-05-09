ea2a81cccd0286480f2490284d79e870556f1607
Nat <nat@cmmc.io>
2017-05-09 16:47:24 +0700

    save MAC address to SPIFFS.

--- ESP_AsyncFSBrowser/ESP_AsyncFSBrowser.ino
+++ ESP_AsyncFSBrowser/ESP_AsyncFSBrowser.ino
@@ -8,11 +8,56 @@
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFSEditor.h>
+#include <ArduinoJson.h>
 
 // SKETCH BEGIN
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 AsyncEventSource events("/events");
+
+bool saveConfig(String mac);
+bool loadConfig() {
+  File configFile = SPIFFS.open("/config.json", "r");
+  if (!configFile) {
+    Serial.println("Failed to open config file");
+    return false;
+  }
+
+  size_t size = configFile.size();
+  if (size > 1024) {
+    Serial.println("Config file size is too large");
+    return false;
+  }
+
+  // Allocate a buffer to store contents of the file.
+  std::unique_ptr<char[]> buf(new char[size]);
+
+  // We don't use String here because ArduinoJson library requires the input
+  // buffer to be mutable. If you don't use ArduinoJson, you may as well
+  // use configFile.readString instead.
+  configFile.readBytes(buf.get(), size);
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& json = jsonBuffer.parseObject(buf.get());
+
+  if (!json.success()) {
+    Serial.println("Failed to parse config file");
+    return false;
+  }
+
+  const char* mac = json["mac"];
+  String macStr = String(mac);
+  uint8_t mac_addr[6];
+  for (size_t i = 0; i < 12; i+=2) {
+    String mac = macStr.substring(i, i+2);
+    byte b = strtoul(mac.c_str(), 0, 16);
+    mac_addr[i/2] = b;
+  }
+  printMacAddress(mac_addr);
+
+  return true;
+}
+
 void printMacAddress(uint8_t* macaddr) {
   Serial.print("{");
   for (int i = 0; i < 6; i++) {
@@ -59,30 +104,11 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
       }
       Serial.printf("[[[ o ]]] MESSAGE => %s\n",msg.c_str());
       String header = msg.substring(0, 7);
-      if (header == "MASTER:") {
-        Serial.print("VALID: ");
-        Serial.println(msg.substring(7));
-        String macStr = msg.substring(7);
-
-        // macStr.substring(0, 2);
-        // macStr.substring(2, 4);
-        // macStr.substring(4, 6);
-        // macStr.substring(6, 8);
-        // uint32_t b = strtoul(string, 0, 16);
-        for (size_t i = 0; i < 12; i+=2) {
-          String mac = macStr.substring(i, i+2);
-          Serial.printf("MAC: [%d] ", i);
-          Serial.println(mac);
-          byte b = strtoul(mac.c_str(), 0, 16);
-          Serial.print("[HEX] ");
-          Serial.println(b, HEX);
-        }
-
-        // Serial.printf("%s-%s-%s-%s-%s-%s", macStr)
-        // uint8_t macTemp[6];
-        // uint8_t b = strtoul(string, 0, 16);
-        // strtoul(const char *__n, char **__end_PTR, int __base)
-        // Serial.println()
+      String value = msg.substring(7);
+      bool validMessage = 0;
+      if (header == "MASTER:" && value.length() == 12) {
+        String macStr = value;
+        saveConfig(macStr);
       }
       else {
         Serial.print("INVALID:");
@@ -131,11 +157,27 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
 }
 
 
-const char* ssid = "NAzT";
-const char* password = "espertap";
+const char* ssid = "poonnawatcafe";
+const char* password = "poonnawat16";
 const char * hostName = "esp-async";
 const char* http_username = "admin";
 const char* http_password = "admin";
+
+
+bool saveConfig(String mac) {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+  json["mac"] = mac;
+
+  File configFile = SPIFFS.open("/config.json", "w");
+  if (!configFile) {
+    Serial.println("Failed to open config file for writing");
+    return false;
+  }
+
+  json.printTo(configFile);
+  return true;
+}
 
 void setup(){
   Serial.begin(115200);
@@ -172,6 +214,8 @@ void setup(){
   MDNS.addService("http","tcp",80);
 
   SPIFFS.begin();
+  loadConfig();
+
 
   ws.onEvent(onWsEvent);
   server.addHandler(&ws);
